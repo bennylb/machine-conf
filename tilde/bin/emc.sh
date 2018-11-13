d525731b14395c2deb8c88a1744ca75cdde43625
Ben Backhouse <bennylb0@gmail.com>
2018-11-14 08:22:23 +1100

    Remove SSH_AUTH_SOCK set duplicate

--- tilde/bin/emc.sh
+++ tilde/bin/emc.sh
@@ -3,8 +3,4 @@
 ES_SOCK="/tmp/emacs1000/server"
 EC_BIN="/home/ben/bin/emc-wrap"
 
-if [ -z "$SSH_AUTH_SOCK" ]; then
-    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-fi
-
 $EC_BIN -n -c -a "" --socket-name=$ES_SOCK "$@"
