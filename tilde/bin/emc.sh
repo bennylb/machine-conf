892552358662c243c9f7a74342d197e99cbd2c54
Ben Backhouse <bennylb0@gmail.com>
2018-11-14 07:06:09 +1100

    Wrap emacs in SSH_AUTH_SOCK

--- tilde/bin/emc.sh
+++ tilde/bin/emc.sh
@@ -3,4 +3,8 @@
 ES_SOCK="/tmp/emacs1000/server"
 EM_CLIENT="/run/current-system/sw/bin/emacsclient"
 
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+fi
+
 $EM_CLIENT -n -c -a "" --socket-name=$ES_SOCK "$@"
