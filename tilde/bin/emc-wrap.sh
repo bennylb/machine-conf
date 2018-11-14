35c8daab41ab68609b6e03a893411db3c5bd24bf
Ben Backhouse <bennylb0@gmail.com>
2018-11-14 17:32:27 +1100

    emc-wrap cleanup

--- tilde/bin/emc-wrap.sh
+++ tilde/bin/emc-wrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+EC_BIN=/run/current-system/sw/bin/emacsclient
+
 if [ -z "$SSH_AUTH_SOCK" ]; then
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 fi
 
-exec /run/current-system/sw/bin/emacsclient "$@"
+exec "$EC_BIN" "$@"
