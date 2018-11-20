c72ecbfdde526796a8954e87440c1cffe6363382
Ben Backhouse <bennylb0@gmail.com>
2018-11-20 23:06:28 +1100

    Fetch clipster from remote fork

--- config/nixpkgs/overlays/clipster.nix
+++ config/nixpkgs/overlays/clipster.nix
@@ -1,5 +1,11 @@
 self: super: {
 
-clipster = super.clipster.overrideAttrs (oldAttrs: { src = /home/ben/src/git/clipster; });
-
+  clipster = super.clipster.overrideAttrs (oldAttrs: {
+    src = super.fetchFromGitHub {
+      owner = "bennylb";
+      repo = "clipster";
+      rev = "5c8acb39722c8d5d0f42cf2ecda2e1f1dac2596b";
+      sha256 = "1kifnkxy0sh9h1xhchwc5haa541czrddq01j9kky9klaxmzlhkd1",
+    };
+  });
 }
