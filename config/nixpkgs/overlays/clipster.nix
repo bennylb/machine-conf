67032af87e390eb8fdcb978fd524a6984bbb97e3
Ben Backhouse <bennylb0@gmail.com>
2018-11-20 23:10:05 +1100

    Fix syntax

--- config/nixpkgs/overlays/clipster.nix
+++ config/nixpkgs/overlays/clipster.nix
@@ -5,7 +5,7 @@ self: super: {
       owner = "bennylb";
       repo = "clipster";
       rev = "5c8acb39722c8d5d0f42cf2ecda2e1f1dac2596b";
-      sha256 = "1kifnkxy0sh9h1xhchwc5haa541czrddq01j9kky9klaxmzlhkd1",
+      sha256 = "1kifnkxy0sh9h1xhchwc5haa541czrddq01j9kky9klaxmzlhkd1";
     };
   });
 }
