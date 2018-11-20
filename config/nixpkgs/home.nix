e9de3638585cedfb4eaf6bf852bda08da8735b60
Ben Backhouse <bennylb0@gmail.com>
2018-11-20 21:29:22 +1100

    Cleaup home.nix

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -60,7 +60,7 @@ in rec {
   };
 
   services = {
-    gnome-keyring.enable = true;
+    # gnome-keyring.enable = true;
     network-manager-applet.enable = true;
   };
 
@@ -73,23 +73,25 @@ in rec {
     #         hpfix = homePrefix;
     #         infix = interfixPath;}));
 
-    file.".nixpkgs/config.nix".source = "${repoRoot}/config.nix";
-    file.".zshrc".source = "${repoRoot}/zshrc";
-    file.".commonrc.sh".source = "${repoRoot}/commonrc.sh";
-    file.".profile".source = "${repoRoot}/profile";
-    file.".bash_profile".source = "${repoRoot}/bash_profile";
-    file.".zprofile".source = "${repoRoot}/zprofile";
-    file.".tmux.conf".source = "${repoRoot}/tmux.conf";
-    file.".xsession".source = "${repoRoot}/xsession";
-    file.".Xresources".source = "${repoRoot}/Xresources";
-    file.".fehbg".source = "${repoRoot}/fehbg";
-    file."bin/emc".source = "${repoRoot}/bin/emc.sh";
-    file."bin/emd".source = "${repoRoot}/bin/emd.sh";
-    file."bin/emw".source = "${repoRoot}/bin/emw.sh";
-    file."bin/emc-wrap".source = "${repoRoot}/bin/emc-wrap.sh";
-    file."bin/restart-emacs".source = "${repoRoot}/bin/restart-emacs.sh";
-    file."bin/touchpad".source = "${repoRoot}/bin/touchpad.sh";
-    file."bin/qutebrowser".source = "${repoRoot}/bin/qutebrowser.sh";
+    file = {
+      ".nixpkgs/config.nix".source = "${repoRoot}/config.nix";
+      ".zshrc".source = "${repoRoot}/zshrc";
+      ".commonrc.sh".source = "${repoRoot}/commonrc.sh";
+      ".profile".source = "${repoRoot}/profile";
+      ".bash_profile".source = "${repoRoot}/bash_profile";
+      ".zprofile".source = "${repoRoot}/zprofile";
+      ".tmux.conf".source = "${repoRoot}/tmux.conf";
+      ".xsession".source = "${repoRoot}/xsession";
+      ".Xresources".source = "${repoRoot}/Xresources";
+      ".fehbg".source = "${repoRoot}/fehbg";
+      "bin/emc".source = "${repoRoot}/bin/emc.sh";
+      "bin/emd".source = "${repoRoot}/bin/emd.sh";
+      "bin/emw".source = "${repoRoot}/bin/emw.sh";
+      "bin/emc-wrap".source = "${repoRoot}/bin/emc-wrap.sh";
+      "bin/restart-emacs".source = "${repoRoot}/bin/restart-emacs.sh";
+      "bin/touchpad".source = "${repoRoot}/bin/touchpad.sh";
+      "bin/qutebrowser".source = "${repoRoot}/bin/qutebrowser.sh";
+    };
 
     # packages = [];
   };
@@ -97,29 +99,33 @@ in rec {
   xdg = {
     enable = true;
 
-    configFile."alacritty/alacritty.yml".source = "${repoRoot}/config/alacritty/alacritty.yml";
-    configFile."clipster/clipster.ini".source = "${repoRoot}/config/clipster/clipster.ini";
-    configFile."i3/config".source = "${repoRoot}/config/i3/config";
-    configFile."i3/pystatus.py".source = "${repoRoot}/config/i3/pystatus.py";
-    configFile."mpv/mpv.conf".source = "${repoRoot}/config/mpv/mpv.conf";
-    configFile."mpv/scripts/dev/select-ao.lua".source = "${repoRoot}/config/mpv/scripts/dev/select-ao.lua";
-    configFile."qutebrowser/bookmarks/urls".source = "${repoRoot}/config/qutebrowser/bookmarks/urls";
-    configFile."qutebrowser/config.py".source = "${repoRoot}/config/qutebrowser/config.py";
-    configFile."qutebrowser/quickmarks".source = "${repoRoot}/config/qutebrowser/quickmarks";
-    configFile."direnv/direnvrc".source = "${repoRoot}/example-nix/tools/direnv/direnvrc";
-    configFile."doom" = {
-      source = "${repoRoot}/config/doom";
-      recursive = true;
-    };
-    configFile."nixpkgs/overlays" = {
-      source = "${repoRoot}/config/nixpkgs/overlays";
-      recursive = true;
+    configFile = {
+      "alacritty/alacritty.yml".source = "${repoRoot}/config/alacritty/alacritty.yml";
+      "clipster/clipster.ini".source = "${repoRoot}/config/clipster/clipster.ini";
+      "i3/config".source = "${repoRoot}/config/i3/config";
+      "i3/pystatus.py".source = "${repoRoot}/config/i3/pystatus.py";
+      "mpv/mpv.conf".source = "${repoRoot}/config/mpv/mpv.conf";
+      "mpv/scripts/dev/select-ao.lua".source = "${repoRoot}/config/mpv/scripts/dev/select-ao.lua";
+      "qutebrowser/bookmarks/urls".source = "${repoRoot}/config/qutebrowser/bookmarks/urls";
+      "qutebrowser/config.py".source = "${repoRoot}/config/qutebrowser/config.py";
+      "qutebrowser/quickmarks".source = "${repoRoot}/config/qutebrowser/quickmarks";
+      "direnv/direnvrc".source = "${repoRoot}/example-nix/tools/direnv/direnvrc";
+      "doom" = {
+        source = "${repoRoot}/config/doom";
+        recursive = true;
+      };
+      "nixpkgs/overlays" = {
+        source = "${repoRoot}/config/nixpkgs/overlays";
+        recursive = true;
+      };
     };
 
-    dataFile."qutebrowser/userscripts/org_capture".source = "${repoRoot}/share/qutebrowser/userscripts/org_capture";
-    dataFile."qutebrowser/userscripts/yank_org_link".source = "${repoRoot}/share/qutebrowser/userscripts/yank_org_link";
-    dataFile."zsh/site-functions/prompt_pure_setup".source = "${repoRoot}/pure/pure.zsh";
-    dataFile."zsh/site-functions/async".source = "${repoRoot}/pure/async.zsh";
+    dataFile = {
+      "qutebrowser/userscripts/org_capture".source = "${repoRoot}/share/qutebrowser/userscripts/org_capture";
+      "qutebrowser/userscripts/yank_org_link".source = "${repoRoot}/share/qutebrowser/userscripts/yank_org_link";
+      "zsh/site-functions/prompt_pure_setup".source = "${repoRoot}/pure/pure.zsh";
+      "zsh/site-functions/async".source = "${repoRoot}/pure/async.zsh";
+    };
   };
 
   gtk = {
