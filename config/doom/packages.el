6582472d543c347305ea568a12ac2fde0b20c4a8
Ben Backhouse <bennylb0@gmail.com>
2018-12-08 05:23:01 +1100

    Pin cquery package until fixed upstream

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -2,3 +2,7 @@
 ;;; ~/machine-conf/config/doom/packages.el
 
 ;; (package! pdf-tools :ignore t)
+
+;; Pin at below commit until cquery restructure is complete.
+;; (package! cquery :recipe (:fetcher git :url "https://github.com/cquery-project/emacs-cquery.git" :commit "a803e92"))
+(package! cquery :recipe (:fetcher github :repo "cquery-project/emacs-cquery" :commit "a803e92"))
