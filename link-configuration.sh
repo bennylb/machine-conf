0ed6cf5244dee543a30bc25d8001569623bacad3
Ben Backhouse <bennylb0@gmail.com>
2018-12-11 03:36:29 +1100

    Small refactor and cleanup

--- link-configuration.sh
+++ link-configuration.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+NIX_CONF=/etc/nixos/configuration.nix
+
 if [ "$EUID" -eq 0 ]; then
-    if test -e /etc/nixos/configuration.nix; then
-        cp /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
+    if test -e "$NIX_CONF"; then
+        cp "$NIX_CONF" "$NIX_CONF".bak
         rm -f /etc/nixos/configuration.nix
     fi
-    ln -s ./sputnik.nix /etc/nixos/configuration.nix
+    ln -s ./sputnik.nix "$NIX_CONF"
 else
     echo "Must be run as root."
     exit 1
