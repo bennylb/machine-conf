fcff25309fc8c66335ff87e1594c3357b129550e
Ben Backhouse <bennylb0@gmail.com>
2018-12-22 07:40:30 +1100

    Continue generalising home configuration

--- sputnik/home.nix
+++ sputnik/home.nix
@@ -3,8 +3,6 @@
 let
   lib      = pkgs.stdenv.lib;
   homeDir  = builtins.getEnv "HOME";
-  emacsDir = "${homeDir}/.emacs.d";
-  doomDir  = "${emacsDir}";
   repoName = "machine-conf";
   repoPath = "${homeDir}/${repoName}";
 in rec {
@@ -26,20 +24,7 @@ in rec {
 
   home = {
     file = {
-      ".nixpkgs/config.nix".source = "${repoPath}/config.nix";
-      ".bashrc".source = "${repoPath}/bashrc";
-      ".zshrc".source = "${repoPath}/zshrc";
-      ".shrc".source = "${repoPath}/shrc";
-      ".profile".source = "${repoPath}/profile";
-      ".bash_profile".source = "${repoPath}/bash_profile";
-      ".zprofile".source = "${repoPath}/zprofile";
-      ".tmux.conf".source = "${repoPath}/tmux.conf";
-      ".xprofile".source = "${repoPath}/xprofile";
-      ".xsession".source = "${repoPath}/xsession";
-      ".Xresources".source = "${repoPath}/Xresources";
       ".fehbg".source = "${repoPath}/fehbg";
-      ".gnupg/gpg-agent.conf".source = "${repoPath}/gnupg/gpg-agent.conf";
-      ".gnupg/gpg.conf".source = "${repoPath}/gnupg/gpg.conf";
     };
   };
 
@@ -51,21 +36,14 @@ in rec {
       "i3/config".source = "${repoPath}/config/i3/config";
       "i3/pystatus.py".source = "${repoPath}/config/i3/pystatus.py";
       "i3/rsstatus.toml".source = "${repoPath}/config/i3/rsstatus.toml";
-      "mpv/mpv.conf".source = "${repoPath}/config/mpv/mpv-sputnik.conf";
-      "mpv/scripts/dev/select-ao.lua".source = "${repoPath}/config/mpv/scripts/dev/select-ao.lua";
       "qutebrowser/bookmarks/urls".source = "${repoPath}/config/qutebrowser/bookmarks/urls";
       "qutebrowser/config.py".source = "${repoPath}/config/qutebrowser/config.py";
       "qutebrowser/quickmarks".source = "${repoPath}/config/qutebrowser/quickmarks";
-      "direnv/direnvrc".source = "${repoPath}/example-nix/tools/direnv/direnvrc";
-      "doom".source = "${repoPath}/config/doom";
-  };
     };
 
     dataFile = {
       "qutebrowser/userscripts/org_capture".source = "${repoPath}/share/qutebrowser/userscripts/org_capture";
       "qutebrowser/userscripts/yank_org_link".source = "${repoPath}/share/qutebrowser/userscripts/yank_org_link";
-      "zsh/site-functions/prompt_pure_setup".source = "${repoPath}/pure/pure.zsh";
-      "zsh/site-functions/async".source = "${repoPath}/pure/async.zsh";
     };
   };
 
--- station/home.nix
+++ station/home.nix
@@ -3,8 +3,6 @@
 let
   lib      = pkgs.stdenv.lib;
   homeDir  = builtins.getEnv "HOME";
-  emacsDir = "${homeDir}/.emacs.d";
-  doomDir  = "${emacsDir}";
   repoName = "machine-conf";
   repoPath = "${homeDir}/${repoName}";
 in rec {
@@ -21,19 +19,7 @@ in rec {
 
   home = {
     file = {
-      ".nixpkgs/config.nix".source = "${repoPath}/config.nix";
-      ".bashrc".source = "${repoPath}/bashrc";
-      ".zshrc".source = "${repoPath}/zshrc";
-      ".shrc".source = "${repoPath}/shrc";
-      ".profile".source = "${repoPath}/profile";
-      ".bash_profile".source = "${repoPath}/bash_profile";
-      ".zprofile".source = "${repoPath}/zprofile";
-      ".tmux.conf".source = "${repoPath}/tmux.conf";
-      ".xprofile".source = "${repoPath}/xprofile";
-      ".xsession".source = "${repoPath}/xsession";
-      ".Xresources".source = "${repoPath}/Xresources";
-      ".gnupg/gpg-agent.conf".source = "${repoPath}/gnupg/gpg-agent.conf";
-      ".gnupg/gpg.conf".source = "${repoPath}/gnupg/gpg.conf";
+
     };
   };
 
@@ -41,15 +27,11 @@ in rec {
     enable = true;
 
     configFile = {
-      "mpv/mpv.conf".source = "${repoPath}/config/mpv/mpv-station.conf";
-      "mpv/scripts/dev/select-ao.lua".source = "${repoPath}/config/mpv/scripts/dev/select-ao.lua";
-      "direnv/direnvrc".source = "${repoPath}/example-nix/tools/direnv/direnvrc";
-      "doom".source = "${repoPath}/config/doom";
+
     };
 
     dataFile = {
-      "zsh/site-functions/prompt_pure_setup".source = "${repoPath}/pure/pure.zsh";
-      "zsh/site-functions/async".source = "${repoPath}/pure/async.zsh";
+
     };
   };
 }
